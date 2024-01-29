efd0e0cd2f0f79fd109c5a3c794972c2badd35c0
superDevOpsGuy <158044243+superDevOpsGuy@users.noreply.github.com>
2024-01-29 12:25:55 +0000

    added Jenkins IAC module'

---
+++ Jenkins/provider.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  profile = "vinay"
+  region  = "ap-south-1"
+}
+
+terraform {
+  required_version = ">=1.7.1"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=5.31.0"
+    }
+  }
+}
