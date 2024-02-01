b1cc96d9a09cd585f13bfe137bcb1b9bfe64c934
superDevOpsGuy <158044243+superDevOpsGuy@users.noreply.github.com>
2024-02-01 17:42:21 +0530

    Added VPC module (#1)
    * Added VPC module
    
    * Update provider.tf
    
    * Update provider.tf
    
    * Update main.tf
    
    * Update main.tf

---
+++ vpc/provider.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  shared_credentials_files = ["/root/.aws/credentials"]
+  region  = "ap-south-1"
+}
+
+terraform {
+  required_version = ">=1.7.2"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=5.31.0"
+    }
+  }
+}
