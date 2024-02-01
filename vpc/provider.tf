713b1e68304d7061962f1186654919a72098e1d5
superDevOpsGuy <158044243+superDevOpsGuy@users.noreply.github.com>
2024-02-01 17:13:50 +0530

    Update provider.tf

--- vpc/provider.tf
+++ vpc/provider.tf
@@ -1,14 +1,14 @@
 provider "aws" {
-  profile = "vinay"
+  shared_credentials_files = ["/root/.aws/credentials"]
   region  = "ap-south-1"
 }
 
 terraform {
-  required_version = ">=1.7.1"
+  required_version = ">=1.7.2"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">=5.31.0"
+      version = ">=5.35.0"
     }
   }
 }
